6addc2d20406f8788eaa9483437b62a0a2adcceb
ErikM06 <murer.erik@gmail.com>
2022-04-13 17:19:53 +0200

    feat!:Refactor TourGuideServicefor a better respect of MVC and microService + created new Exceptions

---
+++ TourGuide/src/main/java/tourGuide/DAO/UserDao.java
@@ -0,0 +1,42 @@
+package tourGuide.DAO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import tourGuide.repository.InternalTestService;
+import tourGuide.customExceptions.UserNotFoundException;
+import tourGuide.user.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserDao {
+
+    Logger logger = LoggerFactory.getLogger(UserDao.class);
+
+    private final InternalTestService internalTestService;
+
+    public UserDao(InternalTestService internalTestService){
+        this.internalTestService = internalTestService;
+    }
+
+    public User getUserFromUserName (String userName) throws UserNotFoundException {
+        return internalTestService.getUser(userName);
+    }
+    public void initializeFromInternalTestService (){
+        logger.info("TestMode enabled");
+        logger.debug("Initializing users");
+        this.internalTestService.initializeInternalUsers();
+        logger.debug("Finished initializing users");
+    }
+    public List<User> getAllUsers() {
+        return internalTestService.internalUserMap.values().stream().collect(Collectors.toList());
+    }
+
+    public void addUser(User user) {
+        if (!internalTestService.internalUserMap.containsKey(user.getUserName())) {
+            internalTestService.internalUserMap.put(user.getUserName(), user);
+        }
+    }
+}
---
+++ TourGuide/src/main/java/tourGuide/customExceptions/UserNotFoundException.java
@@ -0,0 +1,2 @@
+package tourGuide.customExceptions;public class UserNotFoundException {
+}
